7eef20f1106176d774b500915b47fc237a41d9f7
lstgh <lstgh@pc>
2023-03-19 01:38:18 +0600

    added more code for layout and logic

--- SimpleFileBackup/MainWindow.xaml.cs
+++ SimpleFileBackup/MainWindow.xaml.cs
@@ -13,8 +13,11 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Storage;
+using Windows.Storage.Pickers;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -31,7 +34,62 @@ namespace SimpleFileBackup
             this.InitializeComponent();
         }
 
-        private void Start(ItemClickEventArgs e);
+        private async void RootButton_Click(object sender, RoutedEventArgs e)
+        {
+            var window = new Window();
+            var hwnd = WinRT.Interop.WindowNative.GetWindowHandle(window);
+            var folderPicker = new FolderPicker();
+            folderPicker.FileTypeFilter.Add("*");
+            folderPicker.SuggestedStartLocation = PickerLocationId.ComputerFolder;
+            WinRT.Interop.InitializeWithWindow.Initialize(folderPicker, hwnd);
+            var rootFolder = await folderPicker.PickSingleFolderAsync();
+            if (rootFolder != null)
+            {
+                RootButton.Content = rootFolder.DisplayName;
+            }
+        }
 
+        private void ScanButton_Click(object sender, RoutedEventArgs e)
+        {
+            // if InitialScan(no text file present)
+                // Do initialscan
+            // else (if file present)
+                // Compare properties with a doc and live file system
+                // Show changed folder
+        }
+        private void StartButton_Click(object sender, RoutedEventArgs e)
+        {
+            // foreach changed folder
+                // foreach item of it
+                    // copy file from A to B
+        }
+        private void StopButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Wait/Stop current copy process
+            // Stop all threads
+        }
+
+        private async Task<IReadOnlyList<IStorageItem>> ScanRootFolder(StorageFolder folder)
+        {
+            var result = await folder.GetItemsAsync();
+            return result;
+        }
+
+        private void ShowFolders(IReadOnlyList<IStorageItem> item)
+        {
+            
+        }
+        private void ShowFiles()
+        {
+            
+        }
+
+        public enum FileStates
+        {
+            Unchanged = 0,
+            Changed = 1,
+            Deleted = 2,
+            Added = 3
+        }
     }
 }
