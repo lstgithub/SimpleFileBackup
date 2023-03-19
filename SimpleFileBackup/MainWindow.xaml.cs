52023fde2639a7ced057b67460e13334765e5404
lstgh <lstgh@pc>
2023-03-19 17:18:55 +0600

    added more core logic

--- SimpleFileBackup/MainWindow.xaml.cs
+++ SimpleFileBackup/MainWindow.xaml.cs
@@ -2,20 +2,11 @@
 // Licensed under the MIT License.
 
 using Microsoft.UI.Xaml;
-using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
 using System.Threading.Tasks;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 using Windows.Storage;
 using Windows.Storage.Pickers;
 
@@ -34,62 +25,124 @@ namespace SimpleFileBackup
             this.InitializeComponent();
         }
 
+        private readonly string _versionFilePath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "\\" +
+                                                   "SimpleFileBackupVersionHistory.txt";
+        private StorageFolder _rootPath;
+        public List<string> DeletedFiles = new List<string>();
+        public List<string> ModifiedFiles = new List<string>();
+        public List<string> AddedFiles = new List<string>();
+
         private async void RootButton_Click(object sender, RoutedEventArgs e)
         {
             var window = new Window();
-            var hwnd = WinRT.Interop.WindowNative.GetWindowHandle(window);
+            var windowId = WinRT.Interop.WindowNative.GetWindowHandle(window);
             var folderPicker = new FolderPicker();
             folderPicker.FileTypeFilter.Add("*");
             folderPicker.SuggestedStartLocation = PickerLocationId.ComputerFolder;
-            WinRT.Interop.InitializeWithWindow.Initialize(folderPicker, hwnd);
-            var rootFolder = await folderPicker.PickSingleFolderAsync();
-            if (rootFolder != null)
+            WinRT.Interop.InitializeWithWindow.Initialize(folderPicker, windowId);
+            _rootPath = await folderPicker.PickSingleFolderAsync();
+            if (_rootPath != null)
             {
-                RootButton.Content = rootFolder.DisplayName;
+                RootButton.Content = _rootPath.DisplayName;
             }
         }
 
         private void ScanButton_Click(object sender, RoutedEventArgs e)
         {
-            // if InitialScan(no text file present)
-                // Do initialscan
-            // else (if file present)
-                // Compare properties with a doc and live file system
-                // Show changed folder
+            if (CheckForFirstScan())
+            {
+                CreateVersionFile();
+                WriteInitialVersionFile();
+            }
+            else
+            {
+                CollectChanges();
+            }
         }
         private void StartButton_Click(object sender, RoutedEventArgs e)
         {
-            // foreach changed folder
-                // foreach item of it
-                    // copy file from A to B
+
         }
         private void StopButton_Click(object sender, RoutedEventArgs e)
         {
-            // Wait/Stop current copy process
-            // Stop all threads
+
         }
 
-        private async Task<IReadOnlyList<IStorageItem>> ScanRootFolder(StorageFolder folder)
+        private List<string> GetAllFiles()
         {
-            var result = await folder.GetItemsAsync();
-            return result;
+            string path = _rootPath.Path;
+            return Directory.GetFiles(path, "*", SearchOption.AllDirectories).ToList();
         }
 
-        private void ShowFolders(IReadOnlyList<IStorageItem> item)
+        private bool CheckForFirstScan()
         {
-            
-        }
-        private void ShowFiles()
-        {
-            
+            if (!File.Exists(_versionFilePath))
+                return true;
+            return false;
         }
 
-        public enum FileStates
+        private void WriteInitialVersionFile()
         {
-            Unchanged = 0,
-            Changed = 1,
-            Deleted = 2,
-            Added = 3
+            File.WriteAllLines(_versionFilePath, CollectFiles());
+        }
+
+        private List<string> CollectFiles()
+        {
+            var rawAllFiles = GetAllFiles();
+            var allFiles = new List<string>();
+            foreach (var file in rawAllFiles)
+            {
+                FileInfo fi = new FileInfo(file);
+                var modifiedDate = fi.LastWriteTime.ToShortDateString() + " " + fi.LastWriteTime.ToShortTimeString();
+                allFiles.Add(file + " || " + modifiedDate);
+            }
+            return allFiles;
+        }
+
+        private void CollectChanges()
+        {
+            var originalList = File.ReadAllLines(_versionFilePath);
+            var currentList = CollectFiles();
+            var differences = new List<string>();
+            IEnumerable<string> diff = originalList.Except(currentList);
+            foreach (string s in diff)
+            {
+                differences.Add(s);
+            }
+
+            var diffNames = new List<string>();
+            foreach (var file in differences)
+            {
+                diffNames.Add(GetFileNameFromList(file));
+            }
+
+            foreach (var file in diffNames)
+            {
+                if (originalList.Contains(file) && currentList.Contains(file))
+                {
+                    ModifiedFiles.Add(file);
+                }
+                if (originalList.Contains(file) && !currentList.Contains(file))
+                {
+                    DeletedFiles.Add(file);
+                }
+                if (!originalList.Contains(file) && currentList.Contains(file))
+                {
+                    AddedFiles.Add(file);
+                }
+
+            }
+        }
+
+        private string GetFileNameFromList(string s)
+        {
+            var t = s.Split(" || ");
+            return t.First();
+        }
+
+        private void CreateVersionFile()
+        {
+            File.Create(_versionFilePath).Close();
         }
     }
 }
